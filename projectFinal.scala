fb087dfbf152a87cdedc8178cbbab00e885d6930
Munkhbold <munkhbold.d@nmtec.co>
2020-03-14 18:26:26 -0500

    update

--- projectFinal.scala
+++ projectFinal.scala
@@ -8,8 +8,8 @@ def isDouble(input: String): Boolean = Try(input.toDouble).isSuccess
 def getCyl(in:String) : String = { in.split(",")(2)}
 def getMpg(in:String) : String = { in.split(",")(1)}
 
-def summationMean(p: Double, v: Double, qty:Int): (Double, Int) = (p + v, qty + 1)
-def summationVariance(p: Double, v: Double, qty:Int) : (Double, Int) = (p + (v * v), qty + 1)
+def summationMean(p: Double, v: Double, qty: Int): (Double, Int) = (p + v, qty + 1)
+def summationVariance(p: Double, v: Double, qty: Int) : (Double, Int) = (p + (v * v), qty + 1)
 def calcStanDev(variance: Double, mean: Double): Double = scala.math.sqrt(variance - scala.math.pow(mean, 2)).toDouble
 
 // step 2
@@ -34,14 +34,14 @@ def computeAll(dataList:List[(String, Double)]) {
     for( (k,v) <- meanData){
         var row = varianceData(k)
         var mean = v._1/v._2
-        
+
         var variance = calcStanDev(row._1/row._2, mean)
         println(k + " " + mean + " " + scala.math.sqrt(variance.toDouble))
     }
 }
 
 // step 4
-val dataList = cleanData.sample(false, 1).collect().toList
+val dataList = cleanData.sample(false, 0.25).collect().toList
 
 def bootstrapping(dataList:List[(String, Double)], n: Int){
     val meanDic = collection.mutable.Map[String, (Double, Int)]()
@@ -49,7 +49,7 @@ def bootstrapping(dataList:List[(String, Double)], n: Int){
     val dataSet = sc.parallelize(dataList)
     // step 5
     for( i <- 1 to n){
-        var s = dataSet.sample(false, 1).collect().toList
+        var s = dataSet.sample(true, 1).collect().toList
         for( i <- s){
             var k = i._1
             var v = i._2
@@ -74,4 +74,4 @@ def bootstrapping(dataList:List[(String, Double)], n: Int){
 }
 
 computeAll(dsList)
-bootstrapping(dataList, 100)
+bootstrapping(dataList, 1000)
